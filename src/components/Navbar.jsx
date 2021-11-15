d65a0fcdb4ccbc9812c964cecb2003657ca7fdc1
adityasharma2oo2 <adityasharma9319635@gmail.com>
2021-11-16 02:12:59 +0530

    Creating NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import { Button,Menu, Typography, Avatar}
+import { Button,Menu, Typography, Avatar} from antd;
+import {Link} from 'react-router-dom';
+import {HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className = "nav-container">
+      <div className = "logo-container">
+        <Avatar/>
+        <Typograpghy.Title level = {2} className = "logo">
+          <Link to = "/">Cryptoverse</Link>
+        </Typograpghy.Title>
+        {/* <Button className = "menu-control-container">
+
+        </Button> */}
+      </div>
     </div>
   )
 }
