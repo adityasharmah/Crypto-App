e03f8b7fa9fc6a9910da9db665ded43cb284b783
adityasharma2oo2 <adityasharma9319635@gmail.com>
2021-12-17 00:50:49 +0530

    fetch data and 10 top cryptos:

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -6,15 +6,33 @@ import {Card,Row, Col, Input} from  'antd';
 import {useGetCryptoQuery} from '../services/cryptoApi';
 
 
-const Cryptocurrencies = () => {
-  const{ data: cryptoList, isFetching, } = useGetCryptoQuery();
-  const { Cryptos, setCryptos } = useState(cryptoList?.data?.coins);
+const Cryptocurrencies = ({simplified}) => {
+  const count = simplified ? 10 : 100;
+  const{ data: cryptosList, isFetching, } = useGetCryptoQuery(count);
+  const { Cryptos, setCryptos } = useState(cryptosList?.data?.coins);
 
   console.log(cryptos);
   return (
-    <div>
-      Cryptocurrencies
-    </div>
+    <>
+       <Row gutter={[32,32]} className='crypto-card-container'>
+         {cryptos.map((currency) => (
+           <Col xs={24} sm={12} lg={6} className='crypto-card' key = {currency.id}>
+             <Link to ={`/crypto/${currency.id}`}>
+               <Card 
+                  title = {`${currency.rank}. ${currency.name}`}
+                  extra = {<img className = "crypto-image" src = {currency.iconUrl}/>}
+                  hoverable
+                >
+                  <p>Price: {millify(currency.price)}</p>
+                  <p>Market Cap: {millify(currency.marketCap)}</p>
+                  <p>Daily Change: {millify(currency.change)}%</p>
+               </Card>
+             </Link>
+           </Col>
+         ))}
+       </Row>
+
+    </>
   )
 }
 
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -13,7 +13,7 @@ const {Title} = Typography;
 
  
 const Homepage = () => {
-  const {data, isFetching } = useGetCryptosQuery();
+  const {data, isFetching } = useGetCryptosQuery(10);
 
   const globalStats = data?.data?.stats;
 
@@ -37,7 +37,7 @@ const Homepage = () => {
         <Title level = {2} className = "home-title" > Top 10 Cryptocurrencies in the world</Title>
         <Title level = {3} className = "show-more" ><Link to ="/cryptocurrencies">Show More</Link></Title>
       </div>
-      <Cryptocurrencies simplified/>
+      <Cryptocurrencies simplified />
 
       <div className = "home-heading-container">
         <Title level = {2} className = "home-title" >Latest Crypto News</Title>
