25b85ebb2e85a498a48ffd8f3ff2bd88251aca02
adityasharma2oo2 <adityasharma9319635@gmail.com>
2021-12-17 00:24:34 +0530

    add cryptocurrencies

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState }from 'react'
+import millify from 'millify';
+import { Link } from 'react-router-dom';
+import {Card,Row, Col, Input} from  'antd';
+
+import {useGetCryptoQuery} from '../services/cryptoApi';
+
 
 const Cryptocurrencies = () => {
+  const{ data: cryptoList, isFetching, } = useGetCryptoQuery();
+  const { Cryptos, setCryptos } = useState(cryptoList?.data?.coins);
+
+  console.log(cryptos);
   return (
     <div>
       Cryptocurrencies
