499ccb5e1f6f18b7023816b605fc6048146977be
adityasharma2oo2 <adityasharma9319635@gmail.com>
2021-12-23 00:25:40 +0530

    adding search particular news of crypto

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import , {useState} from 'react'
 import {Select,Typography, Row, Col, Avatar,Card} from 'antd';
 import moment from 'moment';
 
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
+import {useGetCryptoQuery} from '../services/cryptoApi';
 
 const { Text, Title } = Typography;
 const { Option } = Select;
@@ -11,19 +12,36 @@ const demoImage = 'http://coinrevolution.com/wp-content/uploads/2020/06/cryptone
 
 
 const News = ({simpilified}) => {
-  const { data : cryptoNews } = useGetCryptoNewsQuery({newsCategory: 'Cryptocurrency',count : simplified ? 6 : 129 })
+  const [newsCategory, setNewsCategory] = useState(CryptoCurrency);
+  const { data : cryptoNews } = useGetCryptoNewsQuery({newsCategory,count : simplified ? 6 : 12 })
+  const { data } =useGetCryptoQuery(100)
 
   if(!cryptoNews?.value) return 'Loading...'
   return (
     <div>
       <Row gutter={[24,24]}>
+        {!simplified && (
+          <Col span = {24}>
+            <Select
+            showSearch
+            className='select-news'
+            placeholder = "Select a Crypto"
+            optionFilterProp = "children"
+            onChange = {(value) => setNewsCategory(value)}
+            filterOption = {(input,option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+            >
+              <Option value = "Cryptocurrency">Cryptocurrency</Option>
+              {data?.data?.coins.map((coin) => <Option value = {coin.name}>{coin.name}</Option></Option>)}
+            </Select>
+          </Col>
+        )}
         {cryptoNews.value.map((news, i) =>(
           <Col xs ={24} sm={24} md={12} lg={8} key={i}>
             <Card hoverable className='news-card'>
               <a href = {news.url} target = "_blank" rel = "noreferrer">
                 <div className='news-image-container'>
                   <Title className='news-title' level = {4}>{news.name}</Title>
-                  <img src = {news?.image?.thumbnail?.contentUrl || demoImage} alt ="news"/>
+                  <img style  = {{maxWidth: '200px', maxHeight = '100px'}}src  = {news?.image?.thumbnail?.contentUrl || demoImage} alt ="news"/>
                 </div>
                 <p>
                   {news.description > 100 
