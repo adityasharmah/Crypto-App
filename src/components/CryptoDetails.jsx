3033deb6654c79d56221bd00077d79dec2888c47
adityasharma2oo2 <adityasharma9319635@gmail.com>
2021-12-25 23:59:12 +0530

    crypto details typography

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import HTMLReactParser from 'html-react-parser'CryptoDetails;
-import { useParams} from 'react-router-dom';
+import {useParams} from 'react-router-dom';
 import {Col, Row, Typography, Select }  from 'antd';
+import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
 
+const { Title,Text } = Typography;
+const {Option}
 
 const CryptoDetails = () => {
   return (
