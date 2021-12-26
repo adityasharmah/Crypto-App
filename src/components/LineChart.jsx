affb10c9cbc0a7d154b2ec87aeccd6881c7a2233
adityasharma2oo2 <adityasharma9319635@gmail.com>
2021-12-26 16:14:14 +0530

    linechart coinprice push

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -6,15 +6,23 @@ const { Title } = Typography;
 
 
 const LineChart = ({ coinHistory, currentPrice, coinName}) => {
+  const coinPrice = [];
+  const Timestamp = [];
+
+  for(let i = 0; i < coinHistory?.data?.history?.length; i += 1){
+    coinPrice.push(coinHistory.data.history[i].price)
+    coinPrice.push(coinHistory.data.history[i].price)
+  }
   return (
     <>
       <Row className='chart-header'>
         <Title level = {2} className='chart-title'>{coinName} Price Chart</Title>
         <Col className='price-container'>
           <Title level = {5} className='price-change'>{coinHistory?.data?.change}%</Title>
-          <Title level = {5} className='price-change'>{coinHistory?.data?.change}%</Title>
+          <Title level = {5} className='current-price'>Current{coinName}Price : ${currentPrice?.data?.change}%</Title>
         </Col>
       </Row>
+      <Line data  = {data} options = {options} ></Line>
 
     </>
   )
